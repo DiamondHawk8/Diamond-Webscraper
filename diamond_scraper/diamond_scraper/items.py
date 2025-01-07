53a921d16e1a3c25ff7353eecf2d3cc519c15cdf
DiamondHawk8 <87952194+DiamondHawk8@users.noreply.github.com>
2025-01-07 15:02:20 -0500

    testing done

--- diamond_scraper/diamond_scraper/items.py
+++ diamond_scraper/diamond_scraper/items.py
@@ -7,6 +7,11 @@ import scrapy
 
 
 class DiamondScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
+    date = scrapy.Field()
+
+
+class StockItem(scrapy.Item):
     pass
