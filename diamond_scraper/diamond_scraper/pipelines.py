91ff158391c93ad9e4338e4833a70a2bb99ef1df
DiamondHawk8 <DiamondHawk8@users.noreply.github.com>
2025-02-20 18:17:04 -0500

    StatTracker Class outline

--- diamond_scraper/diamond_scraper/pipelines.py
+++ diamond_scraper/diamond_scraper/pipelines.py
@@ -160,5 +160,41 @@ class InvalidDataPipeline:
         spider.crawler.stats.set_value("custom/flagged_fields", flagged)
 
 
-def update_stat():
-    pass
+class StatTracker:
+    def __init__(self, spider, enable_logging = False, pipline_name = None, logging_rules = None):
+        self.spider = spider
+
+        # Determines if the class will handle logging itself
+        self.enable_logging = enable_logging
+        self.pipline_name = pipline_name
+
+        default_logging_rules = {}
+        if logging_rules:
+            # TODO allow for partially filled provided rules that are substituted with default where blank
+            self.logging_rules = logging_rules
+
+    # Method for incrementing stat by given amount
+    def increment_stat(self, stat_name: str, value: int = 1):
+        pass
+
+    # Method for appending to a given stat
+    def append_to_stat(self, stat_name: str, data: dict | list):
+        pass
+
+    # Method for getting a stat
+    def get_stat(self, stat_name: str, default=None):
+        pass
+
+    # Determines whether an item should be dropped based on failed rules.
+    def should_drop_item(self, failed_validations: dict) -> bool:
+        # TODO allow for thresholds
+        pass
+
+    def log_validation_results(self, failed_validations: dict, log_level="warning"):
+        # TODO, allow finer control over logging levels
+        pass
+
+    # returns failed validations
+    def validate_item(self, item: dict, rules: dict[str, callable]) -> dict:
+        # TODO, allow for universal rules to be defined and rules that apply to specific keys
+        pass
